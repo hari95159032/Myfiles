05263329718ea8a25fbf1b8fc16ea78222deee88
KALLA HARIKRISHNA <126045869+hari95159032@users.noreply.github.com>
2024-02-23 19:20:24 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import Count from './Components/Count.js';
-import EmployeeClass from './Components/EmployeeClass.js';
-// import UserClass from './Components/UserClass.js';
 
-import 'bootstrap/dist/css/bootstrap.css';
-
+import {Routes,Route} from 'react-router-dom';
+import Header from './Components/Header';
+import Homepage from './Components/Homepage';
+import UserPage from './Components/UserPage';
+import About from './Components/About';
 function App() {
   return (
     <div className="App">
-      <Count />
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Homepage/>}></Route>
+        <Route path='/user' element={<UserPage></UserPage>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
-    
   );
 }
+
 
 export default App;
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,19 +1,15 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-// import CData from "./CData";
-// import CInfo from "./CInfo";
-import { Link } from "react-router-dom";
-
+import React from'react';
+import { useNavigate } from 'react-router-dom';
 let Homepage=()=>{
+    const navigate = useNavigate();
     return(
-        <div id="clshome" >
-            <nav>
-                <Link className="p1" to='/page1'>C_Data</Link>
-                <Link className="p2" to='/page2'>C_Info</Link>
-            </nav>
+        
+        <>
+            <h1>This is HomePage</h1>
            
-            <Outlet></Outlet>
-        </div>
-    )
+            <button class="btnmain" onClick={()=>navigate('/about')}>About</button>
+        
+        </>
+    );
 }
 export default Homepage;
